2917ca67d39b8e3c33c8dbe0625296ce798c951b
Ricard Fredin <ricard@opendia.se>
2015-07-10 10:53:29 +0200

    Add polyfill for CustomEvent (ie9/ie10/ie11).

--- src/lory.js
+++ src/lory.js
@@ -88,6 +88,30 @@ var mergeOptions = function (opts, defaultOptions) {
     return options;
 };
 
+/**
+ * Polyfill for creating CustomEvents on IE9/10/11
+ *
+ * code pulled from:
+ * https://github.com/d4tocchini/customevent-polyfill
+ */
+try {
+    new CustomEvent('test');
+} catch(e) {
+    var CustomEvent = function(event, params) {
+          var evt;
+          params = params || {
+              bubbles: false,
+              cancelable: false,
+              detail: undefined
+          };
+          evt = document.createEvent('CustomEvent');
+          evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+          return evt;
+    };
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent; // expose definition to window
+}
+
 var lory = function (slider, opts) {
     var position;
     var slidesWidth;
