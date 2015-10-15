9ce9a7b2264951b7fc887dc4bc31d6819e129a82
Maximilian Heinz <maximilian.heinz@sinnerschrader.com>
2015-10-15 16:36:20 +0200

    fix(karma tests): add further tests for prev and next (public api) & add saucelabs

---
+++ local.karma.conf.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = function (config) {
+    config.set({
+        frameworks: [
+            'mocha',
+            'chai',
+            'fixture'
+        ],
+
+        reporters: [
+            'mocha'
+        ],
+
+        files: [
+            'dist/lory.js',
+            'test/lory.test.js',
+            'demo/app.css',
+
+            {
+                pattern: 'test/*.html'
+            }
+        ],
+
+        browsers: [
+            'Chrome'
+            // 'ChromeCanary',
+            // 'PhantomJS',
+            // 'Firefox',
+            // 'Opera',
+            // 'Safari'
+        ],
+
+        plugins: [
+            'karma-mocha',
+            'karma-mocha-reporter',
+            'karma-chai',
+            'karma-fixture',
+            'karma-chrome-launcher',
+            'karma-phantomjs-launcher',
+            'karma-ie-launcher',
+            'karma-opera-launcher',
+            'karma-firefox-launcher',
+            'karma-safari-launcher',
+            'karma-html2js-preprocessor'
+        ],
+
+        preprocessors: {
+            'test/*.html': ['html2js']
+        },
+
+        singleRun: true
+    });
+};
