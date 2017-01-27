e1fdbdd3b985a91f19e6fd70046a8d1f0eead073
Jordi Collell <jordic@gmail.com>
2017-01-27 06:09:23 +0100

    fix: race condition on dom setup

--- src/lory.js
+++ src/lory.js
@@ -241,9 +241,7 @@ export function lory (slider, opts) {
         // There is a race here, and when called size of elements is
         // still not initialized, we can delay reset execution, till
         // js next frame. (This will ensure, that size is ready for checking)
-        setTimeout(()=>{
-            reset();
-        }, 0);
+        setTimeout(reset, 0);
 
         if (classNameActiveSlide) {
             setActiveElement(slides, index);
